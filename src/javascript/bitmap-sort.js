118d8e4ca4df8de45c9b4ebb4df8c487965aa303
thinkreed <thinkreed2017@outlook.com>
2017-02-06 20:21:07 +0800

    add bitmap sort

---
+++ src/javascript/bitmap-sort.js
@@ -0,0 +1,32 @@
+function bitmapSort(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  let tempArr = new Array();
+  for (let i = 0; i < max - min + 1; i++) {
+    tempArr[i] = 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let index = arr[i] - min;
+    tempArr[index] += 1;
+  }
+  let index = 0;
+  for (let i = 0; i < tempArr.length; i++) {
+    while (tempArr[i] > 0) {
+      arr[index] = min + i;
+      index++;
+      tempArr[i]--;
+    }
+  }
+  console.log(arr);
+}
+
+var arr = [5, 3, 4, 4, 1];
+bitmapSort(arr);
